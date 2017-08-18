414e5c277a2719999baad9cf91f04f27a1ecc599
Jie Xun See <jiexun.see@mclaren.com>
2017-08-18 10:32:53 +0100

    renamed mat file

--- dev/plotRunResults.m
+++ dev/plotRunResults.m
@@ -1,14 +1,19 @@
-load('RMSE log.mat')
+load('RMSE log 10 dim 3000 n.mat')
 
 data = [diff_old; diff_full; diff_sparse]';
 averages = mean(data, 1);
 stddevs = std(data, 0, 1);
 
-figure;
+figure(1);
 set(gcf, 'Units', 'normalized', 'Position', [0.1, 0.1, 0.8, 0.8])
 b = bar(data);
-l{1} = strcat('Old GP: mu=', num2str(averages(1)), ', sigma=', num2str(stddevs(1))); 
-l{2} = strcat('New full GP: mu=', num2str(averages(2)), ', sigma=', num2str(stddevs(2)));
-l{3} = strcat('New sparse GP: mu=', num2str(averages(3)), ', sigma=', num2str(stddevs(3)));
+l{1} = strcat('Old GP: \mu=', num2str(averages(1),2), ', \sigma=', num2str(stddevs(1),2)); 
+l{2} = strcat('New full GP: \mu=', num2str(averages(2),2), ', \sigma=', num2str(stddevs(2),2));
+l{3} = strcat('New sparse GP: \mu=', num2str(averages(3),2), ', \sigma=', num2str(stddevs(3),2));
+ylabel('RMSE');
+xlabel('Trial number');
+legend(b,l,'FontSize',14);
+% legend({'One','Two','Three','Four'},'FontSize',14)
 
-legend(b,l);
+% figure(2);
+% set(gcf, 'Units', 'normalized', 'Position', [0.1, 0.1, 0.8, 0.8])
