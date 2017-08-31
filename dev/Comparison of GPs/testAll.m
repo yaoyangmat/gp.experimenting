af4f98ea84fc551ade188f75cbf239379f7c4914
Jie Xun See <jiexun.see@mclaren.com>
2017-08-31 10:46:43 +0100

    minor

--- dev/Comparison of GPs/testAll.m
+++ dev/Comparison of GPs/testAll.m
@@ -21,16 +21,16 @@ M = dist_n_train/1000;
 %% RUN AND SAVE
 fgp_history = run(full_MAX_NUM_EVAL, full_n_train, n_test, n_dim, n_responses, 'runFullUF1');
 sgp_history = run(sparse_MAX_NUM_EVAL, sparse_n_train, n_test, n_dim, n_responses, 'runSparseUF1', n_sparse);
-dgp_history = run(dist_MAX_NUM_EVAL, dist_n_train, n_test, n_dim, n_responses, 'runDistUF1', M);
+% dgp_history = run(dist_MAX_NUM_EVAL, dist_n_train, n_test, n_dim, n_responses, 'runDistUF1', M);
 
-save('..\Test results\all_new_results_20.mat', 'fgp_history', 'sgp_history', 'dgp_history')
+save('..\Test results\fgp_sgp_results_20.mat', 'fgp_history', 'sgp_history')
 
 end
 
 %% Helper
 function history = run(MAX_NUM_EVAL, n_train, n_test, n_dim, n_responses, func_name, m)
     for i=1:length(n_train)
-        if strcmp(func_name, 'runFull')
+        if strcmp(func_name, 'runFullUF1')
             [ rmse, time ] = feval( func_name, MAX_NUM_EVAL, n_train(i), n_test, n_dim, n_responses );
         else
             [ rmse, time ] = feval( func_name, MAX_NUM_EVAL, n_train(i), n_test, n_dim, n_responses, m(i) );
